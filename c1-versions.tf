75e691eba3277ee3a747119c5e817beabd4ccdc5
polus8 <58257707+polus8@users.noreply.github.com>
2023-09-10 17:26:51 +0200

    Add files via upload

---
+++ c1-versions.tf
@@ -0,0 +1,14 @@
+/* terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      #version = "~> 4.65" # Optional but recommended in production
+    }
+  }
+}
+*/
+# Provider Block
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
---
+++ c2-ec2instance.tf
@@ -0,0 +1,10 @@
+# Resource: EC2 Instance
+resource "aws_instance" "myec2vm" {
+  ami = "ami-051f7e7f6c2f40dc1"
+  instance_type = "t2.micro"
+  user_data = file("${path.module}/app1-install.sh")
+  tags = {
+    "Name" = "EC2 Demo"
+  }
+}
+
